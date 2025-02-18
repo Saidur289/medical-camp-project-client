516499b1fb64a466187688faa56742b9ef411c8c
Saidur289 <saidur.riaz1@gmailcomm>
2025-02-19 01:13:34 +0600

    create chart

--- src/pages/Dashboard/Organizer/Overview.jsx
+++ src/pages/Dashboard/Organizer/Overview.jsx
@@ -1,5 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Legend,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+  } from "recharts";
+import Loading from "../../../components/shared/Loading/Loading";
 const Overview = () => {
     
     const {data:stats, isLoading} = useQuery({
@@ -10,6 +20,7 @@ const Overview = () => {
             return data
         }
     })
+    if(isLoading) return <Loading></Loading>
     console.log(stats);
   return (
     <div className="bg-[#eef1fd] md:min-h-screen md:flex flex-col justify-center items-center">
@@ -32,6 +43,18 @@ const Overview = () => {
         
         </div>
       </div>
+      <div className="md:w-1/2">
+                <ResponsiveContainer width="100%" height={250}>
+                  <BarChart data={stats}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="totalUsers" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="totalParticipant" fill="#91ee4c" />
+                  </BarChart>
+                </ResponsiveContainer>
+                </div>
     </div>
   );
 };
