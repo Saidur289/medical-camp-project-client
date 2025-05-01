fdd8174f7adfccd9d49e0740e598d90a21177f50
Saidur289 <saidur.riaz1@gmailcomm>
2025-05-01 22:16:35 +0600

    fixed some ui issue some pages

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -41,14 +41,10 @@ import { useThemeStore } from '../../hooks/useThemeStore';
 
 const SettingsPage = () => {
     const {theme, setTheme} = useThemeStore()
-    const PREVIEW_MESSAGES = [
-        { id: 1, content: "Hey! How's it going?", isSent: false },
-        { id: 2, content: "I'm doing great! Just working on some new features.", isSent: true },
-      ];
-      
+   
     return (
-        <div className="h-screen container mx-auto px-4 pt-20 max-w-5xl">
-        <div className="space-y-6">
+        <div className="h-screen container mx-auto px-4  max-w-5xl">
+        <div className="">
           <div className="flex flex-col gap-1">
             <h2 className="text-lg font-semibold">Theme</h2>
             <p className="text-sm text-base-content/70">Choose a theme for your chat interface</p>
